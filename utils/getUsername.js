cb8bc6e01870d6483c8022673a0d7da740aa13bb
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-06-26 21:22:30 +0500

    done getUsername func

--- utils/getUsername.js
+++ utils/getUsername.js
@@ -1,3 +1,7 @@
-const getUsername = (args) => {};
+const getUsername = (args) => {
+  const usernames = args.filter((arg) => arg.match(/^--username=/i));
+  const username = usernames.pop().slice(11);
+  return username;
+};
 
 export { getUsername };
